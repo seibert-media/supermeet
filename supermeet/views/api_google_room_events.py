73d7a2aff107e0e097b13c6966daa7c40f1c0cf0
Franziska Kunsmann <franziska.kunsmann@seibert.group>
2024-05-21 14:12:21 +0200

    do not show event title for private events

--- supermeet/views/api_google_room_events.py
+++ supermeet/views/api_google_room_events.py
@@ -15,19 +15,19 @@ def api_google_room_events(room_id):
     events = {}
     for e in g.get_room_events(CONFIG["rooms"]["google"][room_id]["id"]):
         try:
+            if e.get('summarý') and e.get('visibility') not in ('private', 'confidential'):
+                title = e['summary']
+            elif e['organizer'].get('displayName'):
+                title = e['organizer']['displayName']
+            else:
+                title = e['organizer']['email']
             events[e["start"]["dateTime"]] = {
-                "creator": e["creator"]["email"],
+                "creator": e["organizer"]["email"],
                 "description": e.get("description"),
                 "end": datetime.fromisoformat(e["end"]["dateTime"]).timestamp(),
                 "id": e["id"],
                 "start": datetime.fromisoformat(e["start"]["dateTime"]).timestamp(),
-                "title": e.get(
-                    "summary",
-                    e["creator"].get(
-                        "displayName",
-                        e["creator"]["email"],
-                    ),
-                ).strip(),
+                "title": title.strip(),
             }
         except KeyError as e:
             app.logger.exception('could not add event \'{e.get("id")}\' to list of events')
