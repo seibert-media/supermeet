0ac5dad31d8425a0f2b0d959534554f17f35d93b
Franziska Kunsmann <franziska.kunsmann@seibert.group>
2025-02-04 07:17:27 +0100

    get anny api working

--- supermeet/anny.py
+++ supermeet/anny.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from os import environ
 
 from requests import Session
 
@@ -8,32 +7,92 @@ from . import CONFIG
 
 class AnnyAPI:
     def __init__(self):
+        self.account_id = CONFIG["anny"]["account_id"]
+
+        self._resources = {}
+        self._last_resource_get = datetime.now() - timedelta(days=1)
+
         self.session = Session()
         self.session.headers = {
-            "Authorization": f'Bearer {CONFIG["anny_api_key"]}',
+            "Authorization": f"Bearer {CONFIG['anny']['api_key']}",
         }
 
+    def _paginated_get(self, api_endpoint):
+        data = []
+        if "?" in api_endpoint:
+            next_link = f"{api_endpoint}&page[size]=50&page[number]=1"
+        else:
+            next_link = f"{api_endpoint}?page[size]=50&page[number]=1"
+        while next_link:
+            r = self.__get(next_link)
+            data.extend(r["data"])
+            next_link = r.get("links", {}).get("next")
+        return data
+
     def __get(self, api_endpoint):
-        r = self.session.get(f"https://b.anny.co{api_endpoint}")
+        if not api_endpoint.startswith("https://b.anny.co"):
+            api_endpoint = f"https://b.anny.co/api/v1/{api_endpoint}"
+        r = self.session.get(api_endpoint, timeout=5)
         r.raise_for_status()
         return r.json()
 
-    def get_events(self, start=None, until=None):
+    def get_resources(self):
+        if not self._resources or datetime.now() - self._last_resource_get > timedelta(
+            minutes=10
+        ):
+            resources = {}
+            api_result = self._paginated_get(
+                "resources?filter[exclude_child_resources]=false"
+            )
+            for item in api_result:
+                item_id = int(item["id"])
+                if item["meta"]["parent_id"]:
+                    parent_id = int(item["meta"]["parent_id"])
+                    resources.setdefault(parent_id, {}).setdefault("children", {})
+                    resources[parent_id]["children"][item_id] = item["attributes"][
+                        "name"
+                    ]
+                else:
+                    resources.setdefault(item_id, {})
+                    resources[item_id]["name"] = item["attributes"]["name"]
+                    resources[item_id]["description"] = item["attributes"][
+                        "description"
+                    ]
+                    resources[item_id]["plain_description"] = item["attributes"][
+                        "plain_description"
+                    ]
+
+            self._resources = resources
+            self._last_resource_get = datetime.now()
+        return self._resources
+
+    def get_events(self, start=None, until=None, resources=None):
         if start is None:
             start = datetime.utcnow() - timedelta(days=1)
 
         if until is None:
             until = start + timedelta(days=14)
 
+        resource_filter = ""
+        if resources is not None:
+            resource_filter = (
+                f"&filter[resources]={','.join([str(i) for i in resources])}"
+            )
+
         all_events = []
 
-        events = self.__get(
-            "/api/v1/bookings?include=resource&filter[end_date]={end}&filter[start_date]={start}".format(
+        events = self._paginated_get(
+            "bookings?include=resource&filter[end_date]={end}&filter[start_date]={start}{resource_filter}".format(
                 start=start.isoformat()[:19],
                 end=until.isoformat()[:19],
+                resource_filter=resource_filter,
             )
         )
-        for booking in events["data"]:
+        for booking in events:
             if booking["attributes"]["status"] in ("accepted", "requested", "reserved"):
                 all_events.append(booking)
         return all_events
+
+
+# we need to initialize this here so we can have caching
+anny_api = AnnyAPI()
--- supermeet/views/api_anny_events.py
+++ supermeet/views/api_anny_events.py
@@ -3,31 +3,62 @@ from datetime import datetime
 from flask import abort, jsonify
 
 from .. import CONFIG
-from ..anny import AnnyAPI
+from ..anny import anny_api
 from ..web import app
 
 
-@app.route("/api/anny/<resource>/events/")
-def api_anny_events(resource):
+@app.route("/api/anny/<resource>/details/")
+def api_anny_details(resource):
     if resource not in CONFIG["rooms"]["anny"]:
         abort(404)
-    a = AnnyAPI()
     events = {}
-    for e in a.get_events():
-        if (
-            str(e["relationships"]["resource"]["data"]["id"])
-            not in CONFIG["rooms"]["anny"][resource]["resources"]
-        ):
+    resources = anny_api.get_resources()
+
+    if CONFIG["rooms"]["anny"][resource] not in resources:
+        app.logger.error(
+            f"configured anny room {resource} ({CONFIG['rooms']['anny'][resource]}) not found in anny api result!"
+        )
+        abort(404)
+
+    result = {
+        "name": resources[CONFIG["rooms"]["anny"][resource]]["name"],
+        "description": resources[CONFIG["rooms"]["anny"][resource]]["description"],
+        "children": {
+            child_id: {
+                "name": child_name,
+                "events": [],
+            }
+            for child_id, child_name in resources[CONFIG["rooms"]["anny"][resource]][
+                "children"
+            ].items()
+        },
+    }
+
+    child_events = {
+        child_id: {}
+        for child_id in resources[CONFIG["rooms"]["anny"][resource]]["children"]
+    }
+
+    for e in anny_api.get_events(
+        resources=list(resources[CONFIG["rooms"]["anny"][resource]]["children"])
+    ):
+        try:
+            resource_id = int(e["relationships"]["resource"]["data"]["id"])
+        except KeyError:
+            continue
+        if resource_id not in child_events:
             continue
         description = e["attributes"]["description"]
         if description in ("Flex-Buchung",):
             description = e["attributes"]["note"].splitlines()[0]
-        events[e["attributes"]["start_date"]] = {
+        child_events[resource_id][e["attributes"]["start_date"]] = {
             "end": datetime.fromisoformat(e["attributes"]["end_date"]).timestamp(),
             "id": e["id"],
             "start": datetime.fromisoformat(e["attributes"]["start_date"]).timestamp(),
             "title": description,
         }
 
-    result = [v for k, v in sorted(events.items())]
+    for child_id, events in child_events.items():
+        result["children"][child_id]["events"] = [v for k, v in sorted(events.items())]
+
     return jsonify(result)
